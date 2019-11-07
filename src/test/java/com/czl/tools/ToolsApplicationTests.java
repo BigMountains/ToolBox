13edc9b500e6e6f0b8a595906c25fc85e590b514
大山 <394546541@qq.com>
2019-11-07 10:45:14 +0800

    1. 新增http同步异步请求

--- src/test/java/com/czl/tools/ToolsApplicationTests.java
+++ src/test/java/com/czl/tools/ToolsApplicationTests.java
@@ -2,6 +2,7 @@ package com.czl.tools;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +10,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ToolsApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
 
 }
